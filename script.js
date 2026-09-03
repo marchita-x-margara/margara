e53c8134b8a0ce76d8a979acd977e0b3c3738667
marchita-x-margara <cinnie014@gmail.com>
2026-09-02 22:51:40 -0300

    1

---
+++ script.js
@@ -0,0 +1,25 @@
+const works = document.querySelectorAll(".work");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+
+        entries.forEach((entry) => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("visible");
+
+            }
+
+        });
+
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+
+works.forEach((work) => {
+    observer.observe(work);
+});
